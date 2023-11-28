91b64dc8d471fb712ef2fc7b7e0e32f4cf84316b
Pknashi <pknashi616@gmail.com>
2023-11-28 22:24:19 +0530

    added new branch

--- travello/urls.py
+++ travello/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [#path("", views.index, name="index"),
-                path("", views.ecommerce, name="index"),
+                path("", views.ecommerce, name="ecommerace"),
                path("home", views.ecommerce, name="home"),
                path("travello", views.index, name="travello"),
                path("about", views.about, name="about")]
